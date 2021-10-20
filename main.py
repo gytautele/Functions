9723804df871f5961348b113857688638ea582d1
gytautele <47378732+gytautele@users.noreply.github.com>
2021-10-20 21:01:54 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,48 +1,23 @@
-def modify():
-    global cur, c, accept, flag, att, up, n, name_, apt, st, col, col_n
-    col = ('', '', 'type', 'qty_left', 'cost', 'purpose', 'expdt', 'loc', 'mfg')
-    col_n = ('', '', 'Type', 'Quantity Left', 'Cost', 'Purpose', 'Expiry Date', 'Rack location', 'Manufacture')
-    flag = 'st'
-    name_ = ''
+def stock():
+    global cur, c, columns, accept, flag, sto, apt
     apt.destroy()
-    n = []
-    cur.execute("select * from med")
-    for i in cur:
-        n.append(i[1])
-    c.commit()
-    st = Tk()
-    st.title('MODIFY')
-    Label(st, text='-' * 48 + ' MODIFY DATABASE ' + '-' * 48).grid(row=0, column=0, columnspan=6)
-
-    def onvsb(*args):
-        name_.yview(*args)
-
-    def onmousewheel():
-        name_.ywiew = ('scroll', event.delta, 'units')
-        return 'break'
-
-    cx = 0
-    vsb = Scrollbar(orient='vertical', command=onvsb)
-    vsb.grid(row=1, column=3, sticky=N + S)
-    name_ = Listbox(st, width=43, yscrollcommand=vsb.set)
-    cur.execute("select *from med")
-    for i in cur:
-        cx += 1
-        name_.insert(cx, (str(i[0]) + '.  ' + str(i[1])))
-        name_.grid(row=1, column=1, columnspan=2)
-    c.commit()
-    name_.bind('<MouseWheel>', onmousewheel)
-    name_.bind('<<ListboxSelect>>', sel_mn)
-
-    Label(st, text='Enter Medicine Name: ').grid(row=1, column=0)
-    Label(st, text='Enter changed Value of: ').grid(row=2, column=0)
-    att = Spinbox(st, values=col_n)
-    att.grid(row=2, column=1)
-    up = Entry(st)
-    up.grid(row=2, column=2)
-    Button(st, width=10, text='Submit', bg='green', fg='white', command=save_mod).grid(row=2, column=4)
-    Button(st, width=10, text='Reset', bg='red', fg='white', command=res).grid(row=2, column=5)
-    Button(st, width=10, text='Show data', bg='blue', fg='white', command=show_val).grid(row=1, column=4)
-    Label(st, text='-' * 120).grid(row=3, column=0, columnspan=6)
-    Button(st, width=10, text='Main Menu', bg='green', fg='white', command=main_menu).grid(row=5, column=5)
-    st.mainloop()
+    flag = 'sto'
+    accept = [''] * 10
+    sto = Tk()
+    sto.title('STOCK ENTRY')
+    Label(sto, text='ENTER NEW PRODUCT DATA TO THE STOCK').grid(row=0, column=0, columnspan=2)
+    Label(sto, text='-' * 50).grid(row=1, column=0, columnspan=2)
+    for i in range(1, len(columns)):
+        Label(sto, width=15, text=' ' * (14 - len(str(columns[i]))) + str(columns[i]) + ':').grid(row=i + 2, column=0)
+        accept[i] = Entry(sto)
+        accept[i].grid(row=i + 2, column=1)
+    Button(sto, width=15, text='Submit', bg='blue', fg='white', command=submit).grid(row=12, column=1)
+    Label(sto, text='-' * 165).grid(row=13, column=0, columnspan=7)
+    Button(sto, width=15, text='Reset', bg='red', fg='white', command=reset).grid(row=12, column=0)
+    Button(sto, width=15, text='Refresh stock', bg='skyblue', fg='black', command=ref).grid(row=12, column=4)
+    for i in range(1, 6):
+        Label(sto, text=columns[i]).grid(row=14, column=i - 1)
+    Label(sto, text='Exp           Rack   Manufacturer                      ').grid(row=14, column=5)
+    Button(sto, width=10, text='Main Menu', bg='green', fg='white', command=main_menu).grid(row=12, column=5)
+    ref()
+    sto.mainloop()
